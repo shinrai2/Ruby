dc50935dc47670633373a8ee5bf80ae4db0e8015
shinrai2 <fuck@shinrai.cc>
2016-12-15 23:16:38 +0800

    ss.rb 去除标点

--- ss.rb
+++ ss.rb
@@ -2,7 +2,13 @@
 
 file = File.open("pac.txt")
 text = file.read
+file.close
 tsplit = text.split
+if ARGV[0] then
+	regex1 = Regexp.new(".+" + ARGV[0] + ".+")
+else
+	regex1 = /.+twitter.+/
+end
 tsplit.each do |ts|
-	p ts if /.+google.+/ =~ ts
+	p ts[1, ts.length - 3] if regex1 =~ ts
 end
